8951125ab93f5f2744dfbdb00ce0145d5ecf7bce
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2019-10-08 14:48:17 -0300

    Adding core function of the package

--- index.js
+++ index.js
@@ -3,21 +3,60 @@ var rp = require('request-promise-native');
 var cheerio = require('cheerio');
 var parseString = require('xml2js').parseString;
 
-sorting_possibilities = ["","%20sort_cited:y","%20sort_date:y"]
-query = "dendritic cell"
+async function search_papers(query,n_papers, sorting_type, metadata){
 
-var options = {
-  uri: 'https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=' + query + sorting_possibilities[0] +"&format=json",
-  headers: {
-      'User-Agent': 'Request-Promise'
-  },
-  json: true // Automatically parses the JSON string in the response
-};
 
-rp(options)
-  .then(function (result) {
-      console.log(result.resultList.result[0]);
-  })
-  .catch(function (err) {
-      // API call failed...
-  });
+      if (sorting_type == "citation") {
+      sorting_command = "%20sort_cited:y"
+
+    } else if (sorting_type = "date"){
+      sorting_command = "%20sort_date:y"
+    } else {
+      sorting_command = ""
+      console.log("Sorting by relevance")
+    }
+
+
+    if (metadata=="full"){
+      metadata_command = "&resultType=core"
+    } else {
+      metadata_command = ""
+    }
+
+    var options = {
+      uri: 'https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=' + 
+      query + 
+      sorting_command +
+      metadata_command +
+      '&pageSize=' + n_papers +
+      "&format=json",
+
+      headers: {
+          'User-Agent': 'Request-Promise'
+      },
+      json: true // Automatically parses the JSON string in the response
+    };
+    
+    retobj = rp(options)
+      .then(function (result) {
+
+          fancy_result =  {}
+          fancy_result.count =    result.hitCount
+          fancy_result.papers = result.resultList.result
+          return(fancy_result)
+
+
+
+
+
+      })
+      .catch(function (err) {
+          // API call failed...
+      });
+    
+    return(retobj)
+  
+  }
+  
+
+module.exports = search_papers;
